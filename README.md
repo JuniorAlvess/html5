683eae2181eed508177b1b3d8bb1e9fee31a3d49
Junior Alves <junioralves.jav@gmail.com>
2020-10-08 12:13:53 -0300

    Fist commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# HTML5
+
+## Repositorio focado em estudos baseados em css. 
+
+### O que foi abordado
+
+==================
+
+<!--ts-->
+    * [Anatomia]
+    * [Texto Puro]
+    * [Imagens]
+    * [links]
+    * [Lista Aninhada]
+    * [Tabela]
+    * [Tags não visuais]
+    * [Texto]
+    * [Video]
+    * [Projeto form]
+        * [Criação do formulario]
+        * [Servidor (criado com JavaScript)]
+<!--te-->
---
+++ exercicios/form/server.js
@@ -0,0 +1,25 @@
+// instale o express 'npm i --save express'
+// instale o body-parser 'npm i --save body-parse
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { response } = require('express')
+
+// urlencoded é o padrão que vem apartir da submissão, por isso deve ser feito o parse
+// O body-parser interpreta o urlencoded e cria um objeto preenchido com os dados 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// requisições
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Incluido!!!</h1>')
+})
+
+app.post('/usuario/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alterado!!!</h1>')
+})
+
+app.listen(3003)
